1c420f2e97a5b498de8da6760df4c2e589739a8d
koushik <koushik.dey@live.com>
2022-09-07 19:12:47 +0300

    HTML from website

--- web_scraping.py
+++ web_scraping.py
@@ -1,12 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-url= "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
 
-result = requests.get(url)
-print(result.text)
-
-
+# HTML From File:
 
 #with open("index.html", "r") as f:
     #doc = BeautifulSoup(f, "html.parser")
@@ -17,4 +13,18 @@ print(result.text)
 #tags = doc.find_all("p")
 #tag.string = "hello"
 #print(tags)
+
+
+# HTML From Website
+
+url= "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
+
+result = requests.get(url)
+#print(result.text)
+doc = BeautifulSoup(result.text, "html.parser")
+print(doc.prettify())
+
+
+
+
 
