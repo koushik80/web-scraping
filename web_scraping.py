19c6f6c7a203b13671e183e3142cfdb53cb73f50
koushik <koushik.dey@live.com>
2022-09-07 16:53:27 +0300

    print-tag

--- web_scraping.py
+++ web_scraping.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/dionysus"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+# print(doc.prettify())
+
+tag = doc.title
+print(tag.string)
+
